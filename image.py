ac72366777ea978272f87365358f50f147e2c574
Shane Claussen <sclaussen@yahoo.com>
2022-01-17 15:09:54 -0800

    Added Empty.gif and TicTacToe.psd to the repo

--- image.py
+++ image.py
@@ -8,6 +8,11 @@ o2 = o1.resize((100, 100), Image.ANTIALIAS)
 o3 = ImageTk.PhotoImage(o2)
 Label(root, image=o3).pack()
 
+empty1 = Image.open('images/Empty.gif')
+empty2 = empty1.resize((100, 100), Image.ANTIALIAS)
+empty3 = ImageTk.PhotoImage(empty2)
+Label(root, image=empty3).pack()
+
 x1 = Image.open('images/X.gif')
 x2 = x1.resize((100, 100), Image.ANTIALIAS)
 x3 = ImageTk.PhotoImage(x2)

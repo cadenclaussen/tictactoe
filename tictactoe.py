f76bc30502663df24b75f923b34865eac4edc5b2
Caden Claussen <cadenclaussen@Cadens-Mac-mini.local>
2022-01-17 14:15:22 -0800

    Edits

--- tictactoe.py
+++ tictactoe.py
@@ -2,7 +2,12 @@ import random
 
 def main():
 	board = [[".", ".", "."], [".", ".", "."], [".", ".", "."]]
-	names, token = getPlayerNames()
+	beadsPlayed = 0
+	currentPlayer, names, token = getPlayerNames()
+	while winner == False:
+		move = getMove()
+		winner = anylizeMove(move, currentPlayer, token)
+		currentPlayer, token = nextPlayer(currentPlayer, token, names)
 
 def getPlayerNames():
 	names = []
@@ -10,4 +15,13 @@ def getPlayerNames():
 	names.append(input("What is your name p2? "))
 	return random.choice(names), names, "X"
 
-def getMove(currentPlayer)
+def getMove(currentPlayer):
+	return input("What is your move " + currentPlayer + "? (1 1), (2 1)").split()
+
+def anylizeMove(move, currentPlayer, token):
+	full = True
+	
+def nextPlayer(currentPlayer, token, names):
+	if currentPlayer == names[0]:
+		r
+
